8c0548e842a42858288a838ca053ad21f7152272
Ivan Ilin <ilyin1997@icloud.com>
2023-08-05 18:56:17 +0300

    add statistic

--- src/Generator.java
+++ src/Generator.java
@@ -1,78 +1,80 @@
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Generator {
-    private static final AtomicInteger sumCriterionOne = new AtomicInteger(0);
-    private static final AtomicInteger sumCriterionTwo = new AtomicInteger(0);
-    private static final AtomicInteger sumCriterionTree = new AtomicInteger(0);
+    private static final BlockingQueue<String> blockingQueueWithA = new ArrayBlockingQueue<>(100);
+    private static final BlockingQueue<String> blockingQueueWithB = new ArrayBlockingQueue<>(100);
+    private static final BlockingQueue<String> blockingQueueWithC = new ArrayBlockingQueue<>(100);
 
-    public static void main(String[] args) {
-        Random random = new Random();
-        String[] texts = new String[100_000];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("abc", 3 + random.nextInt(3));
-        }
+    public static void main(String[] args) throws InterruptedException {
+        AtomicReference<String> maxA = new AtomicReference<>("");
+        AtomicReference<String> maxB = new AtomicReference<>("");
+        AtomicReference<String> maxC = new AtomicReference<>("");
+        int a = 0;
+        int b = 0;
+        int c = 0;
 
-        Predicate<String> criterion = (t) -> {
-            if (t.length() == 4) {
-                if (t.startsWith(String.valueOf(t.charAt(0) + t.charAt(1))) ==
-                        t.endsWith(String.valueOf(t.charAt(0) + t.charAt(1)))) {
-                    return true;
+        Thread th = new Thread(() -> {
+            for (int i = 0; i < 10_000; i++) {
+                String text = generateText("abc", 100_000);
+                try {
+                    blockingQueueWithA.put(text);
+                    blockingQueueWithB.put(text);
+                    blockingQueueWithC.put(text);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
                 }
             }
-
-            char[] simvols = t.toCharArray();
-            int check = 0;
-            for (int i = 1; i < simvols.length; i++) {
-                if (simvols[i - 1] == simvols[i]) {
-                    check = 1;
-                } else check = 0;
-            }
-            if (check == 1) return true;
-
-            for (int i = 1; i < simvols.length; i++) {
-                if (simvols[i - 1] < simvols[i]) {
-                    check = 1;
-                } else check = 0;
-            }
-            return check == 1;
-        };
-
-        Thread threadOne = new Thread(() -> runSum(texts, 3, criterion, sumCriterionOne));
-
-        Thread threadTwo = new Thread(() -> runSum(texts, 4, criterion, sumCriterionTwo));
-
-        Thread threadTree = new Thread(() -> runSum(texts, 5, criterion, sumCriterionTree));
-
-        List<Thread> threads = List.of(threadOne, threadTwo, threadTree);
-        threads.forEach(Thread::start);
-        threads.forEach(thread -> {
             try {
-                thread.join();
+                blockingQueueWithA.put("END");
+                blockingQueueWithB.put("END");
+                blockingQueueWithC.put("END");
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        });
 
-        System.out.printf("""
-                        Красивых слов с длиной 3: %s шт
-                        Красивых слов с длиной 4: %s шт
-                        Красивых слов с длиной 5: %s шт
-                        """,
-                sumCriterionOne, sumCriterionTwo, sumCriterionTree);
+        });
+        th.start();
+
+
+        Thread threadMaxA = new Thread(() -> search(blockingQueueWithA, maxA, a, 'a'));
+
+        Thread threadMaxB = new Thread(() -> search(blockingQueueWithB, maxB, b, 'b'));
+        Thread threadMaxC = new Thread(() -> search(blockingQueueWithC, maxC, c, 'c'));
+
+        List<Thread> threads = List.of(threadMaxA, threadMaxB, threadMaxC);
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Строка с максимальным количесвтом символов 'a'\n" + maxA);
+        System.out.println();
+        System.out.println("Строка с максимальным количесвтом символов 'b'\n" + maxB);
+        System.out.println();
+        System.out.println("Строка с максимальным количесвтом символов 'c'\n" + maxC);
+        System.out.println();
     }
 
-    private static void runSum(String[] texts, int x, Predicate<String> criterion, AtomicInteger integer) {
-        for (String text : texts) {
-            if (text.length() == x) {
-                if (criterion.test(text)) {
-                    integer.incrementAndGet();
-                }
+    private static void search(BlockingQueue<String> queue, AtomicReference<String> max, int sum, char simvol) {
+        String text = "";
+        while (!text.equals("END")) {
+            try {
+                text = queue.take();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            int newSum = (int) text.chars().filter((ch) -> ch == simvol).count();
+            if (newSum > sum) {
+                sum = newSum;
+                max.set(text);
             }
         }
     }
+
 
     public static String generateText(String letters, int length) {
         Random random = new Random();
